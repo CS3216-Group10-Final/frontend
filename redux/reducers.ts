80327692836ee68583cc5ef75acd5d1cc8f8079d
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-06 02:26:46 +0800

    Update reducers

--- redux/reducers.ts
+++ redux/reducers.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import user from "./slices/User_slice";
 import gameEntry from "./slices/GameEntry_slice";
+import userStatistics from "./slices/UserStatistics_slice";
 
 const rootReducer = combineReducers({
   user,
   gameEntry,
+  userStatistics,
 });
 
 export default rootReducer;
