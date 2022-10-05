7a2ffc60ee56d3843e2de2e335c25242e8d65bc6
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-06 02:09:06 +0800

    Fix formatting

--- api/user_statistics_api.ts
+++ api/user_statistics_api.ts
@@ -2,7 +2,9 @@ import axiosInstance from "./axios";
 import { getPathForUserStatisticsWithId } from "./endpoint_paths";
 import { UserStatistics } from "./types";
 
-export async function getGameEntryByIdApi(id: number): Promise<UserStatistics> {
+export async function getUserStatisticsByIdApi(
+  id: number
+): Promise<UserStatistics> {
   const response = await axiosInstance.get<UserStatistics>(
     getPathForUserStatisticsWithId(id)
   );
