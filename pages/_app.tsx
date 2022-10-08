5924dbbbd199b0133a36edab8fd63a107b825579
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-09 00:59:05 +0800

    Add colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,32 @@ function MyApp({ Component, pageProps }: AppProps) {
       withGlobalStyles
       withNormalizeCSS
       theme={{
+        colors: {
+          red: [
+            "#f6a1a2",
+            "#f48e8f",
+            "#f27b7d",
+            "#f1686a",
+            "#ef5558",
+            "#ed4245",
+            "#d53b3e",
+            "#be3537",
+            "#a62e30",
+            "#8e2829",
+          ],
+          green: [
+            "#99dbbd",
+            "#84d3af",
+            "#70cca2",
+            "#5bc595",
+            "#47bd87",
+            "#32b67a",
+            "#2da46e",
+            "#289262",
+            "#237f55",
+            "#1e6d49",
+          ],
+        },
         colorScheme: "dark",
         primaryColor: "yellow",
         primaryShade: 6,
