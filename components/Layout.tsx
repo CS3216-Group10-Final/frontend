ba19068488c6ded7a7ecf1696bac2224c74e9b20
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-09 23:25:02 +0800

    Fix bugs

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,17 @@
 import TokenService from "@api/authentication/token_service";
 import { handleApiRequestError } from "@api/error_handling";
 import { AppShell, Container } from "@mantine/core";
-import { useAppDispatch, useAppSelector } from "@redux/hooks";
-import { getSelfUser, selectUser } from "@redux/slices/User_slice";
+import { useAppDispatch } from "@redux/hooks";
+import { getSelfUser } from "@redux/slices/User_slice";
 import React from "react";
 import AppBar from "./AppBar";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
-  const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
   React.useEffect(() => {
     if (
       TokenService.getLocalAccessToken() &&
-      TokenService.getLocalRefreshToken() &&
-      !user
+      TokenService.getLocalRefreshToken()
     ) {
       dispatch(getSelfUser()).catch((error) => {
         console.log(handleApiRequestError(error).errorType);
