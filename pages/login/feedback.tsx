946471c734daa277cfb8b234997fcd4cf25db257
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-15 05:12:52 +0800

    Fix google auth

--- pages/login/feedback.tsx
+++ pages/login/feedback.tsx
@@ -5,15 +5,19 @@
 import TokenService from "@api/authentication/token_service";
 import {
   ErrorType,
+  handleApiRequestError,
   showApiRequestErrorNotification,
 } from "@api/error_handling";
 import { Text } from "@mantine/core";
+import { useAppDispatch } from "@redux/hooks";
+import { getSelfUser } from "@redux/slices/User_slice";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 const GoogleAuthRedirect: NextPage = () => {
   const router = useRouter();
+  const dispatch = useAppDispatch();
   const { refresh, access } = router.query;
 
   useEffect(() => {
@@ -22,14 +26,21 @@ const GoogleAuthRedirect: NextPage = () => {
     }
     if (typeof refresh == "string" && typeof access == "string") {
       TokenService.setTokens({ refreshToken: refresh, accessToken: access });
+      dispatch(getSelfUser())
+        .then(() => {
+          router.push("/");
+        })
+        .catch((error) => {
+          showApiRequestErrorNotification(handleApiRequestError(error));
+        });
     } else {
       showApiRequestErrorNotification({
         errorType: ErrorType.TOKEN_NOT_VALID,
         name: "",
         message: "",
       });
+      router.push("/");
     }
-    router.push("/");
   }, [router.isReady]);
 
   return <Text>Redirecting...</Text>;
