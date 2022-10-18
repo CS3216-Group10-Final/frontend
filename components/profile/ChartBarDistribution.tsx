fbb26a08063e84353486ef600ea9a3811ec2c31b
DystoriaX <dascogabriel@gmail.com>
2022-10-18 22:47:33 +0800

    change chart to pie

--- components/profile/ChartBarDistribution.tsx
+++ components/profile/ChartBarDistribution.tsx
@@ -1,16 +1,47 @@
-import { useMantineTheme, Stack, SegmentedControl } from "@mantine/core";
+import { useMantineTheme, Stack, SegmentedControl, Card } from "@mantine/core";
 import { GameEntryStatus, Genre, Platform } from "@api/types";
 import {
-  BarChart,
-  XAxis,
-  YAxis,
-  Bar,
   ResponsiveContainer,
   Tooltip,
+  PieChart,
+  Cell,
+  Pie,
+  Legend,
+  PieLabelRenderProps,
 } from "recharts";
 import { toProperCase } from "utils/helpers";
 import { useState } from "react";
 import { useMobile } from "utils/useMobile";
+
+const customLabel = (
+  props: PieLabelRenderProps,
+  fontFamily: string | undefined
+) => {
+  const cx = Number(props.cx);
+  const cy = Number(props.cy);
+  const innerRadius = Number(props.innerRadius);
+  const midAngle = Number(props.midAngle);
+  const outerRadius = Number(props.outerRadius);
+  const percent = Number(props.percent);
+
+  const RADIAN = Math.PI / 180;
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return percent > 0.15 ? (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+      fontFamily={fontFamily}
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  ) : null;
+};
 
 type Props = {
   gameStatusDistribution: Partial<Record<GameEntryStatus, number>>;
@@ -41,6 +72,15 @@ const ChartBarDistribution = (props: Props) => {
 
   const theme = useMantineTheme();
   const isMobile = useMobile();
+
+  // can change???
+  const COLORS = [
+    theme.colors.yellow[6],
+    theme.colors.orange[6],
+    theme.colors.green[6],
+    theme.colors.cyan[6],
+    theme.colors.indigo[6],
+  ];
 
   const gameStatusData = Object.entries(gameStatusDistribution).map((entry) => {
     const [key, value] = entry;
@@ -111,18 +151,44 @@ const ChartBarDistribution = (props: Props) => {
     setChartType(value);
   };
 
-  console.log(theme.fontFamily);
-
   return (
-    <Stack align="center" sx={{ width: "100%" }}>
-      <SegmentedControl
-        value={chartType}
-        onChange={handleChange}
-        data={segmentedControlData}
-        size={isMobile ? "xs" : "sm"}
-      />
-      <ResponsiveContainer height={256} width={isMobile ? "100%" : "80%"}>
-        <BarChart
+    <Card radius="lg" shadow="sm" p="xl">
+      <Stack align="center" sx={{ width: "100%" }}>
+        <SegmentedControl
+          value={chartType}
+          onChange={handleChange}
+          data={segmentedControlData}
+          size={isMobile ? "xs" : "sm"}
+        />
+        <ResponsiveContainer height={256} width={isMobile ? "100%" : "80%"}>
+          <PieChart width={256} height={256}>
+            {isMobile || <Tooltip />}
+            <Legend layout="vertical" verticalAlign="middle" align="right" />
+            <Pie
+              data={chartData[chartType]}
+              label={(props) => customLabel(props, theme.fontFamily)}
+              labelLine={false}
+              dataKey="value"
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              animationBegin={0}
+              animationDuration={1000}
+            >
+              {chartData[chartType].map((entry, index) => {
+                return (
+                  <Cell
+                    name={
+                      isMobile ? `${entry.label} - ${entry.value}` : entry.label
+                    }
+                    key={index}
+                    fill={COLORS[index % COLORS.length]}
+                  />
+                );
+              })}
+            </Pie>
+          </PieChart>
+          {/* <BarChart
           data={chartData[chartType]}
           margin={isMobile ? { left: -30, right: 10 } : {}}
         >
@@ -146,9 +212,10 @@ const ChartBarDistribution = (props: Props) => {
             fill={theme.fn.primaryColor()}
             maxBarSize={64}
           />
-        </BarChart>
-      </ResponsiveContainer>
-    </Stack>
+        </BarChart> */}
+        </ResponsiveContainer>
+      </Stack>
+    </Card>
   );
 };
 
