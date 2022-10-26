b7a9c4037535071d326b2a7c9896d276cef50e96
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-10-27 02:24:25 +0800

    Fix profile page headers

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -1,21 +1,32 @@
 import PageHeader from "@components/PageHeader";
 import ProfilePage from "@components/profile";
 import { LoadingOverlay } from "@mantine/core";
-import { NextPage } from "next";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import { useRouter } from "next/router";
 
-const UserProfilePage: NextPage = () => {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { username } = context.query;
+
+  return {
+    props: {
+      username: username as string,
+    },
+  };
+};
+
+const UserProfilePage = ({
+  username,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
-  const { username } = router.query;
 
   return (
     <>
-      {username && (
-        <PageHeader
-          title={username ? `${String(username)}'s Profile` : "Loading..."}
-          description={`View ${String(username)}'s Game Collection `}
-        />
-      )}
+      <PageHeader
+        title={username ? `${String(username)}'s Profile` : "Loading..."}
+        description={`View ${String(username)}'s Game Collection `}
+      />
       <LoadingOverlay visible={!router.isReady} />
       <ProfilePage username={username as string} />
     </>
