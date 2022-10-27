9212f8018048319a93ecb069b8797bbe03c68dd4
DystoriaX <dascogabriel@gmail.com>
2022-10-28 00:57:55 +0800

    game entry: add select component to change status

--- components/profile/GameEntryCard.tsx
+++ components/profile/GameEntryCard.tsx
@@ -1,4 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
+import {
+  handleApiRequestError,
+  showApiRequestErrorNotification,
+} from "@api/error_handling";
 import { GameEntry, GameEntryStatus } from "@api/types";
 import ReviewModal from "@components/ReviewModal";
 import {
@@ -8,20 +12,25 @@ import {
   Group,
   HoverCard,
   Image,
+  Select,
   Stack,
   Text,
   Tooltip,
   useMantineTheme,
 } from "@mantine/core";
 import { useHover } from "@mantine/hooks";
+import { useAppDispatch } from "@redux/hooks";
+import { updateGameEntry } from "@redux/slices/GameEntry_slice";
 import Link from "next/link";
 import { useState } from "react";
 import { TbEdit, TbFileText } from "react-icons/tb";
+import { showSuccessNotification } from "utils/notifications";
 import {
   allPlatformCategories,
   categorizePlatforms,
   getPlatformCategoryIcon,
 } from "utils/platform_categories";
+import { STATUS_DATA } from "utils/status";
 import { useMobile } from "utils/useMobile";
 
 type Props = {
@@ -56,14 +65,43 @@ const GameEntryCard = (props: Props) => {
     [GameEntryStatus.WISHLIST]: theme.colors.dark[3],
   };
 
+  const dispatch = useAppDispatch();
+
+  const handleStatusChange = (newStatus: string) => {
+    console.log(newStatus);
+    console.log(GameEntryStatus[Number(newStatus)]);
+
+    const newGameEntry: GameEntry = {
+      ...gameEntry,
+      status: Number(newStatus),
+    };
+
+    dispatch(updateGameEntry(newGameEntry))
+      .unwrap()
+      .then(() => {
+        showSuccessNotification({
+          title: "Status updated",
+          message: gameEntry.game_name,
+        });
+      })
+      .catch((error) => {
+        showApiRequestErrorNotification(handleApiRequestError(error));
+      });
+  };
+
   return (
     <div>
       <Card
         p={0}
-        style={{ borderLeft: "10px solid", borderColor: badgeColor[status] }}
+        style={{
+          borderLeft: "10px solid",
+          borderColor: badgeColor[status],
+          overflow: "visible",
+        }}
+        mt="lg"
       >
-        <Grid align="center">
-          <Grid.Col span={3} p={0}>
+        <Grid align="center" ml={0}>
+          <Grid.Col span={3} p={0} style={{ overflow: "hidden" }}>
             <Image
               width="100%"
               height="auto"
@@ -73,7 +111,7 @@ const GameEntryCard = (props: Props) => {
               sx={{ maxHeight: 120 }}
             />
           </Grid.Col>
-          <Grid.Col span={isMobile ? 5 : 7} p={0} pl="sm">
+          <Grid.Col span={isMobile ? 5 : 6} p={0} pl="sm">
             <Stack>
               <Link href={`/games/${game_id}`}>
                 <Text
@@ -117,42 +155,67 @@ const GameEntryCard = (props: Props) => {
               </Group>
             </Stack>
           </Grid.Col>
-          <Grid.Col span={isMobile ? 4 : 2} p={0}>
-            <Group sx={{ height: "100%" }} position="right" mr="sm">
-              {review && (
-                <Tooltip label="Read review">
-                  <ActionIcon
-                    onClick={() => {
-                      setReviewModalIsOpen(true);
-                    }}
+          <Grid.Col span={isMobile ? 4 : 3} p={0}>
+            <Stack mr="lg">
+              <Group sx={{ height: "100%" }} position="right">
+                {review && (
+                  <Tooltip label="Read review">
+                    <ActionIcon
+                      onClick={() => {
+                        setReviewModalIsOpen(true);
+                      }}
+                    >
+                      <TbFileText size={18} />
+                    </ActionIcon>
+                  </Tooltip>
+                )}
+                {rating !== undefined && rating !== null && (
+                  <Text
+                    size="md"
+                    align="right"
+                    mr={isEditable ? 0 : "sm"}
+                    weight={500}
                   >
-                    <TbFileText size={18} />
-                  </ActionIcon>
-                </Tooltip>
-              )}
-              {rating !== undefined && rating !== null && (
-                <Text
-                  size="md"
-                  align="right"
-                  mr={isEditable ? 0 : "sm"}
-                  weight={500}
-                >
-                  {rating}/10
-                </Text>
-              )}
-              {isEditable && (
-                <Tooltip label="Edit game entry">
-                  <ActionIcon
-                    mr="sm"
-                    onClick={() => {
-                      onClickEdit && onClickEdit(gameEntry);
-                    }}
-                  >
-                    <TbEdit size={18} />
-                  </ActionIcon>
-                </Tooltip>
-              )}
-            </Group>
+                    {rating}/10
+                  </Text>
+                )}
+                {isEditable && (
+                  <Tooltip label="Edit game entry">
+                    <ActionIcon
+                      onClick={() => {
+                        onClickEdit && onClickEdit(gameEntry);
+                      }}
+                    >
+                      <TbEdit size={18} />
+                    </ActionIcon>
+                  </Tooltip>
+                )}
+              </Group>
+              <Select
+                value={String(status)}
+                data={STATUS_DATA}
+                onChange={handleStatusChange}
+              />
+              {/* <Menu position="bottom-end">
+                <Menu.Target>
+                  <Anchor align="right">Update status</Anchor>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  {Object.keys(GameEntryStatus)
+                    .filter((v) => !isNaN(Number(v)))
+                    .map((value) => {
+                      return (
+                        <Menu.Item key={value}>
+                          {gameEntryStatusToString(
+                            value as unknown as GameEntryStatus
+                          )}
+                        </Menu.Item>
+                      );
+                    })}
+                </Menu.Dropdown>
+              </Menu> */}
+            </Stack>
           </Grid.Col>
         </Grid>
         <ReviewModal
